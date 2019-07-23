780be88088207f6cdef67be3ee574df921dd60ef
Nopekick <tgkamm@yahoo.com>
2019-07-23 14:38:15 -0700

    barebones

--- src/App.js
+++ src/App.js
@@ -3,26 +3,54 @@ import './App.css';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 
-class App extends Component() {
+class App extends Component {
   constructor(props){
+    super(props)
     this.state = {
-
+      ingredients: [],
+      cur: ''
     }
   }
 
+  handleChange = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleAdd = () => {
+    let ingredients = this.state.ingredients.slice()
+    ingredients.push(this.state.cur)
+    this.setState({ingredients, cur: ''})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   render(){
+    let ingredients = this.state.ingredients.map((ing)=>{
+      return <div>
+        <h2> {ing} </h2>
+      </div>
+    })
     return (
       <div>
-        <form onSubmit={}>
-          <Input defaultValue="ingredient..."
-            className={classes.input}
+        {ingredients}
+        <form onSubmit={this.handleSubmit}>
+          <Input onChange={this.handleChange} defaultValue="ingredient..."
             inputProps={{
             'aria-label': 'Description',
-            }}
+            }} name='cur' value={this.state.cur}
           />
-          <Button variant="contained" color="primary" className={classes.button}>
-              Primary
+          <div className='buttons'>
+            <Button type='button' onClick={this.handleAdd} variant="contained" color="primary" >
+                Add Another
+            </Button>
+            <Button type='submit'  variant="contained" color="primary" >
+              Submit
           </Button>
+          </div>
+          
+          
         </form>
 
       </div>
