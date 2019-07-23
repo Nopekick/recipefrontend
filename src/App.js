d94de98b852a98dba1f1a1e1a719224b77fe12b0
Nopekick <tgkamm@yahoo.com>
2019-07-23 14:12:53 -0700

    material ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+  constructor(props){
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={}>
+          <Input defaultValue="ingredient..."
+            className={classes.input}
+            inputProps={{
+            'aria-label': 'Description',
+            }}
+          />
+          <Button variant="contained" color="primary" className={classes.button}>
+              Primary
+          </Button>
+        </form>
+
+      </div>
+    );
+  }
+  
 }
 
 export default App;
