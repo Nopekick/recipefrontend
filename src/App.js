60b8740e458076888746f09fb86c9637fe5c4614
Arjun balaji <arjun37602@gmail.com>
2019-07-24 13:59:44 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
       <div>
         <div className='Heading'> 
           <h1>Recipe App</h1>
-          <p>Welcome to the Recipe App. Here, you can check if the food you are going to cook is seasonal or not. To do this, you simply need to add all the ingredients you are going to use and we'll let you know if those ingredients are seasonal.</p>
         </div>
         <Switch>
           <Route exact path="/" component={()=> <Form setResults={this.setResults} />} />
